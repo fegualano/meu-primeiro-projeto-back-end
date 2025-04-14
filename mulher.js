46da4f9916c3a91275606309067a04ae81d8c700
fegualano <fegualano@gmail.com>
2025-04-14 15:42:14 -0300

    insere a nova versao  do projeto integrado com o mongodb

--- mulher.js
+++ mulher.js
@@ -8,6 +8,7 @@ const porta = 3333
 function mostraMulher(request, response){
  response.json({
     nome: 'Fernanda Gualano',
+    imagem: 'https://media.licdn.com/dms/image/v2/C4D03AQGo9pEAWnA3vw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1580595734784?e=1750291200&v=beta&t=lrhh0aTFrsqslKKkjdcbRxR8M-J4czYxiZ9k5ZEeZJ8',
     minibio: 'Estudante de TI'
  })
 }
